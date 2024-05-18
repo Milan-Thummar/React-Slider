d17a9b953ed0c3599c83dfb84682a2f44d249c03
Milan Thummar <thummar.fb@gmail.com>
2024-05-19 00:43:34 +0200

    Fetch the data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-function App() {
-  return <h1>Slider Project</h1>;
-}
+import React, { useEffect, useState } from "react";
+import { Product } from "./types/Product";
+import Slider from "./components/Slider";
+import { fetchData } from "./components/FetchData";
+import "./styles/App.scss";
+
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    initializeSlider();
+  }, []);
+
+  const initializeSlider = async () => {
+    const data = await fetchData("https://dummyjson.com/products", 5);
+    setProducts(data);
+  };
+
+  return <Slider products={products} />;
+};
 
 export default App;
